59c8842707378dab762071f71d4fab4a8dfab871
Rahimova <aggun.rahimova@adesso-sweden.se>
2025-10-17 13:06:29 +0200

    New parameter

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    x = "Hellooo!!! from Ilhama"
-    return x
+    x = "Hellooo!!! from Ilhama and "
+    y = "Aggun"
+    return x + y
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
