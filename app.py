08ff20963609bf9dc9bc4837513c7e01e4b2703d
Ilhame <qaflanova.i@gmail.com>
2025-10-17 14:08:17 +0300

    added new string

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    x = "Hellooo!!!"
+    x = "Hellooo!!! from Ilhama"
     return x
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
