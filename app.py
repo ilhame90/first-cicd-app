ad5a88b1d7289be2b8ae0ceed5e66af98f748e4d
Ilhame <qaflanova.i@gmail.com>
2025-10-17 14:19:57 +0300

    chnaged x

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    x = "Hellooo!!! from Ilhama"
+    x = "Hellooo!!! from Ilhama test 2 teting 2nd case"
     return x
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
